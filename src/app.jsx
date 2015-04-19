e9c8546e02922b3b02e484994952be12d33a1d9e
vandosant <sjskender@gmail.com>
2015-04-19 16:30:42 -0600

    Added some default properties

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,24 @@
 var React = require("react");
 
 var App = React.createClass({
+  getDefaultProps(){
+    return {
+      text: 'this is a default prop',
+      number: 0
+    }
+  },
+  propTypes: {
+    text: React.PropTypes.string,
+    number: React.PropTypes.number.isRequired
+  },
   render(){
-    return <h1>Hello world</h1>;
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <span>{this.props.text}</span>
+      </div>
+    )
   }
 });
 
-React.render(<App/>, document.getElementById("example"));
+React.render(<App number={5}/>, document.getElementById("example"));
